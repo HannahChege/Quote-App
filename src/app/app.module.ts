be74422a5cb066d0ba5edbbb4cce64d2439318e9
HannahChege <chegehannah45@gmail.com>
2018-08-12 15:19:04 +0300

    Added a new quote1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuotedetailsComponent } from './quotedetails/quotedetails.component';
+import { Quote1Component } from './quote1/quote1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuotedetailsComponent
+    QuotedetailsComponent,
+    Quote1Component
   ],
   imports: [
     BrowserModule
